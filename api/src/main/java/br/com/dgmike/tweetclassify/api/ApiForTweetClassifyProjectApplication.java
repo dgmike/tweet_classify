af43e43239003fb3e45af089dd214715baf7eb04
Michael <contato@dgmike.com.br>
2021-11-28 19:37:06 -0300

    :sparkles: api base structure created

---
+++ api/src/main/java/br/com/dgmike/tweetclassify/api/ApiForTweetClassifyProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.dgmike.tweetclassify.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForTweetClassifyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForTweetClassifyProjectApplication.class, args);
+	}
+
+}
